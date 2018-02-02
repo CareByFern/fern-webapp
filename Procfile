48c8193c801d4dcf2f37a6e0ae7bc53d9500d56c
Noah Tye <hi@noahtye.com>
2018-02-02 13:19:31 -0800

    make staticfiles work with gunicorn

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py collectstatic --noinput;  gunicorn elderserver.wsgi --log-file -
+web: gunicorn elderserver.wsgi --log-file -
--- elderserver/urls.py
+++ elderserver/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views import static
 from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from giver import views
 
@@ -30,3 +31,5 @@ urlpatterns = [
     path('static/', static.serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
